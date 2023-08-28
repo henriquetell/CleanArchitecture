c8139247d65be2ce28d6255c26e63002649d56d5
henriquetelles <67623099+henriquetelles@users.noreply.github.com>
2023-08-29 00:05:48 +0200

    Initial commit

---
+++ Application/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using FluentValidation;
+
+namespace Application;
+
+public static class DependencyInjection
+{
+	public static IServiceCollection AddApplication(this IServiceCollection services)
+	{
+		var assembly = typeof(DependencyInjection).Assembly;
+		services.AddMediatR(configuration => configuration.RegisterServicesFromAssembly(assembly));
+
+		services.AddValidatorsFromAssembly(assembly);
+
+		return services;
+	}
+}
